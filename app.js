14f9248eddce4f9c05ab88d924f9c374ae6eb488
Erick <cdzbr46211@gmail.com>
2025-01-26 12:24:19 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,17 @@
+var nome = "Erick";
+var idade = 36;
+var possuiFaculdade = true;
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobreNome = "Luciano",
+  cidade = "São Paulo";
+
+console.log(sobreNome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
