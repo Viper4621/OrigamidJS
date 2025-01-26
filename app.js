abc39dd8459d5cc2c9168caae094c1e5858ae3e0
Erick <cdzbr46211@gmail.com>
2025-01-26 12:29:01 -0300

    exercicios

--- app.js
+++ app.js
@@ -15,3 +15,30 @@ console.log(sobreNome, cidade);
 
 var semDefinir;
 console.log(semDefinir);
+
+var comida;
+console.log(comida);
+comida = "pizza";
+console.log(comida);
+
+// Declarar uma variável com o seu nome
+
+// Declarar uma variável com a sua idade
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+// Atribuir valor a sua comida favorita
+
+// Declarar 5 variáveis diferentes sem valores
+
+var novoNome = "Jhon";
+var novaIdade = 50;
+var comidaFavorita;
+comidaFavorita = "Milho";
+
+var semValor1;
+var semValor2;
+var semValor3;
+var semValor4;
+var semValor5;
